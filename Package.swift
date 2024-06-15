1dccd1474e5f441b174a8b9981c4ce168d99f56e
designer1的mac <77demac@shejibudeiMac.local>
2024-06-15 13:44:28 +0800

    a1

--- Package.swift
+++ Package.swift
@@ -4,20 +4,26 @@
 import PackageDescription
 
 let package = Package(
-    name: "MyLibrary",
+    name: "LmSDK",
+    platforms: [
+        .iOS(.v13)
+    ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
-            name: "MyLibrary",
-            targets: ["MyLibrary"]),
+            name: "LmSDK",
+            targets: ["LmSDK"]),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/CoderMJLee/MJRefresh.git", from: "3.7.1"),
+        .package(url: "https://github.com/SDWebImage/SDWebImage.git", from: "5.1.0"),
+        .package(url: "https://github.com/jdg/MBProgressHUD.git", .upToNextMajor(from: "1.2.0")),
     ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
-        .target(
-            name: "MyLibrary"),
-        .testTarget(
-            name: "MyLibraryTests",
-            dependencies: ["MyLibrary"]),
+        .binaryTarget(
+            name: "LmSDK",
+            url: "https://github.com/FineaX/LmSDKPackage/releases/download/1.0.0/LmSDK.xcframework.zip",
+            checksum: "c33300b7d21cfc3ddcc142561b7ae8bfb18d35c142fb32760f9bc139ab25eec8"
+        )
     ]
 )
